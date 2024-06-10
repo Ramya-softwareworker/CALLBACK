7f459a013f088c37718f8592b57d72e4e0219631
Ramya-softwareworker <ramya04032001@gmail.com>
2024-06-10 22:12:17 +0530

    Add files via upload

---
+++ CALLBACK/js/script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.querySelector('.countdown').innerHTML= 10;
+    return setTimeout(()=>{
+        document.querySelector('.countdown').innerHTML= 9;
+        return setTimeout(()=>{
+            document.querySelector('.countdown').innerHTML= 8;
+            return setTimeout(()=>{
+                document.querySelector('.countdown').innerHTML= 7;
+                return setTimeout(()=>{
+                    document.querySelector('.countdown').innerHTML= 6;
+                    return setTimeout(()=>{
+                        document.querySelector('.countdown').innerHTML= 5;
+                        return setTimeout(()=>{
+                            document.querySelector('.countdown').innerHTML= 4;
+                            return setTimeout(()=>{
+                                document.querySelector('.countdown').innerHTML= 3;
+                                return setTimeout(()=>{
+                                    document.querySelector('.countdown').innerHTML= 2;
+                                    return setTimeout(()=>{
+                                        document.querySelector('.countdown').innerHTML= 1;
+                                        return setTimeout(()=>{
+ document.querySelector('.countdown').innerHTML= `<span class= "firstword"> HAPPY </span> <span class='secondword'> INDEPENDENCE </span> <span class= 'thirdword'> DAY </span>`;
+             },1000);
+             },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
